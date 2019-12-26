482ffdc070e041342fb523003a7aad4e7f585fe8
emag <lanabe.lanabe@gmail.com>
2019-12-27 05:28:03 +0900

    Done chap04

---
+++ chap04/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", 10 / 3);
+    printf("%d\n", 10 % 3);
+    printf("%f\n", 10.0 / 3.0);
+
+    printf("%d ÷ %d = %d ... %d\n",  40, 13, 40 / 13, 40 % 13);
+    return 0;
+}
